ea80e916499bc59de5a0feed4817f739189d37d5
bokang03 <bogang12345@gmail.com>
2026-03-17 14:09:00 +0900

    apiFetch 함수 추가, api 호출에 관련된 보일러플레이트를 제거

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -3,6 +3,7 @@
 import { PostDto } from "@/type/post";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { fetchApi } from "@/lib/client";
 
 export interface Post {
     id: number,
@@ -15,8 +16,7 @@ export default function Home() {
     const [posts, setPosts] = useState<PostDto[]>([]);
 
     useEffect(() => {
-        fetch("http://localhost:8080/api/v1/posts")
-            .then(response => response.json())
+        fetchApi(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/posts`)
             .then(data => {
                 console.log(data)
                 setPosts(data);
