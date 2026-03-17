0670cff0fcd5a651ba373887736f4465ff1e1e39
bokang03 <bogang12345@gmail.com>
2026-03-17 11:43:14 +0900

    userParams로 게시글 id 등록 -2

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,5 +1,17 @@
+"use client"
+import { useParams } from "next/navigation";
+
 export default function Home() {
+    const {id} = useParams();
     return (
-        <div>1번 글 상세페이지</div>
-    )
+        <>
+        <div className="flex flex-col gap-8 items-center">
+                <h1>{id}번 글 상세페이지</h1>
+                <div>
+                    <h1>제목</h1>
+                    <div>내용</div>
+                </div>
+            </div>
+        </>
+    );
 }
